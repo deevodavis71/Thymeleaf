f27849147e96356f567db4f1c34a49682a05fef2
Steve Davis <davissa@uk.innovation-group.com>
2017-08-07 21:07:23 +0100

    Check model

--- ui/src/main/java/com/airbus/smartdeploy/ui/web/UserInterfaceController.java
+++ ui/src/main/java/com/airbus/smartdeploy/ui/web/UserInterfaceController.java
@@ -1,13 +1,34 @@
 package com.airbus.smartdeploy.ui.web;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
 
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.client.RestTemplate;
+
+import com.airbus.smartdeploy.usermanagement.dto.user.UserDTO;
+
+@Slf4j
 @Controller
 public class UserInterfaceController {
 
 	@RequestMapping("/")
-	public String goHome() {
+	public String goHome(Model model) {
+
+		// Execute the service
+		RestTemplate restTemplate = new RestTemplate();
+        UserDTO[] users = restTemplate.getForObject("http://localhost:10000/api/users", UserDTO[].class);
+
+        List<UserDTO> lstUsers = Arrays.asList(users);      
+        log.info ("User: {}", lstUsers);
+
+		model.addAttribute("users", lstUsers);
+		
 		return "index";
 	}
 
